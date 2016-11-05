7d7f4022bfb66a1cbec25361a96015081b74111e
brunosouzapicinini <brunosouzapicinini@gmail.com>
2016-11-04 22:38:31 -0200

    initial

---
+++ src/java/br/com/modelo/Pedido.java
@@ -0,0 +1,26 @@
+package br.com.modelo;
+
+
+public class Pedido {
+    
+    private int id;
+    private int idUsuario;
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+    
+}
